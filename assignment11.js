c46d50a47e509e81f0c7694193ae864566b6a481
meera <meera.singh52@mail.com>
2018-07-05 18:28:20 +0530

    Assinment 11 added

---
+++ assignment11.js
@@ -0,0 +1,28 @@
+
+// function stringvar (){
+//     var temp = "meera singh"
+//     var t1= temp.split(" ");
+//     var t2= t1[0].charAt(0).toUpperCase();
+//     var t3= t1[1].charAt(0).toUpperCase();
+//     var t4= t2 +t3;
+//     console.log(t1);
+//     console.log(t2);
+//     console.log(t3);
+//     console.log(t4);
+// }
+
+function stringvar ()
+{
+    
+var temp = "akshay singh rathore";
+var t1= temp.split("");
+var t2;
+for(var i=0 ; i<=t1.length-1; i++)
+{
+     t2= t1[i].charAt(0).toUpperCase();
+     console.log(t2);
+    
+}
+}
+stringvar();
+
